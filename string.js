4c71da967987a51d94127bc91bf61395e81f3944
raginigoswami19485 <raginigoswami19485@gmail.com>
2025-01-10 16:35:58 +0530

    first commit

---
+++ string.js
@@ -0,0 +1,18 @@
+VANTA.BIRDS({
+    el: "#doc",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+  })
+ 
+  var typed = new Typed('#type', {
+    strings: ['hello', 'ragini.'],
+    typeSpeed: 50,
+    loop:true
+    
+    // shuffle: true
+  });
